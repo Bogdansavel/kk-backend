59011d41a1ae4629d6898e9172580a7911a85910
bogdansavel <bogdanarsaman@gmail.com>
2024-03-30 11:34:28 +0200

    trigger build

--- src/main/java/com/example/kkbackend/controllers/MainController.java
+++ src/main/java/com/example/kkbackend/controllers/MainController.java
@@ -13,6 +13,7 @@ public class MainController {
     public RedirectView getAuthRequest(@RequestParam String username) {
         RedirectView redirectView = new RedirectView();
         redirectView.setUrl("https://docs.google.com/forms/d/e/1FAIpQLSc1JLQ1Oxcgy7730NoAsJrUI5JhjgXTSUOKnm2bUE-cH6Nm1Q/viewform?usp=pp_url&entry.518733161=@" + username);
+        //trigger build
         return redirectView;
     }
 }
