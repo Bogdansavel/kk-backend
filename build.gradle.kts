94b562e7e810671aca20686c2427681aa6367df6
bogdansavel <bogdanarsaman@gmail.com>
2024-04-26 13:17:24 +0200

    implemented connection to google cloud sql

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("com.google.api-client:google-api-client:2.2.0")
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
     implementation("com.google.apis:google-api-services-sheets:v4-rev612-1.25.0")
+    compileOnly("com.google.cloud.sql:postgres-socket-factory:1.18.0")
 }
 
 tasks.withType<Test> {
