069d0829b6e0814e1da475936ce7cba9cb8d055e
bogdansavel <bogdanarsaman@gmail.com>
2024-04-26 14:18:38 +0200

    remove jdbc-socket-factory-core dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,11 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
     implementation("org.springframework.boot:spring-boot-starter-jdbc:3.2.4")
     implementation("org.postgresql:postgresql:42.7.3")
-    implementation("com.google.api-client:google-api-client:2.2.0")
-    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
-    implementation("com.google.apis:google-api-services-sheets:v4-rev612-1.25.0")
-    implementation("com.google.cloud.sql:jdbc-socket-factory-core:1.18.0")
-    compileOnly("com.google.cloud.sql:postgres-socket-factory:1.18.0")
+    compileOnly("com.google.cloud.sql:postgres-socket-factory:1.4.1")
 }
 
 tasks.withType<Test> {
