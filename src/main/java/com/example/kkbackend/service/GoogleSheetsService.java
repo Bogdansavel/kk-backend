19ce9c47b7fe49b6fcb712aa021fb25f1e4e0ed2
bogdansavel <bogdanarsaman@gmail.com>
2024-06-01 01:27:27 +0200

    change credentials receiver

--- src/main/java/com/example/kkbackend/service/GoogleSheetsService.java
+++ src/main/java/com/example/kkbackend/service/GoogleSheetsService.java
@@ -42,10 +42,9 @@ public class GoogleSheetsService {
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+
+        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
     }
 
 
