3a278166afd05dd34869d497729381e78d2d6961
bogdansavel <bogdanarsaman@gmail.com>
2024-11-24 15:31:54 +0100

    + remove google dependencies from build.gradle.kts + add connection string to test db to application-local.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,11 +28,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
     implementation("org.springframework.boot:spring-boot-starter-jdbc:3.2.4")
     implementation("org.postgresql:postgresql:42.7.3")
-    implementation("com.google.api-client:google-api-client:2.2.0")
-    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
-    implementation("com.google.apis:google-api-services-sheets:v4-rev20240514-2.0.0")
-    implementation("com.google.cloud.sql:postgres-socket-factory:1.18.0")
-    runtimeOnly("com.google.cloud:spring-cloud-gcp-starter-secretmanager:5.3.0")
 }
 
 tasks.withType<Test> {
