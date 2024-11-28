248e3ed4264e5395fd74e61ddc622404b836e9af
hop1n <hopinplay@gmail.com>
2024-11-28 22:05:38 +0100

    added delete rating endpoint

--- src/main/java/com/example/kkbackend/repositories/RateRepository.java
+++ src/main/java/com/example/kkbackend/repositories/RateRepository.java
@@ -12,4 +12,5 @@ import java.util.UUID;
 @Repository
 public interface RateRepository extends JpaRepository<Rate, UUID> {
     Optional<Rate> getRateByMovieAndMember(Movie movie, Member member);
+    void deleteById(UUID uuid);
 }
