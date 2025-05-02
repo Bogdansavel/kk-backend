f728172824450347abf8f90d541381c57c6d490c
bogdan <bogdanarsaman@gmail.com>
2025-05-02 16:53:04 +0200

    update versions of hibernate-core and spring-boot-starter-web

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation("org.springframework.boot:spring-boot-starter-web:3.2.11")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.4.2")
     implementation("org.springframework.boot:spring-boot-maven-plugin:3.2.4")
     implementation("org.springframework.boot:spring-boot-starter-validation:3.2.0")
-    implementation("org.hibernate.orm:hibernate-core:6.4.4.Final")
+    implementation("org.hibernate.orm:hibernate-core:6.6.13.Final")
     implementation("org.springframework.boot:spring-boot-starter-security:3.2.12")
     compileOnly("org.projectlombok:lombok:1.18.30")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
